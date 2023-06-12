046738a325a9587b70470d64c626364276eee32d
Joseph Cooper <cooper.joseph@gmail.com>
2023-06-12 19:53:09 +0100

    chore: Release v0.2

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -63,7 +63,7 @@ java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
 
 group = "com.omricat"
 
-version = "0.1"
+version = "0.2"
 
 application { mainClass.set("com.omricat.sleepystories.server.MainKt") }
 
