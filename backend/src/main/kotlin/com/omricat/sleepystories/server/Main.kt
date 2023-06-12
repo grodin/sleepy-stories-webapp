1eca3774106802762e48f97bcd33d79c8878e659
Joseph Cooper <cooper.joseph@gmail.com>
2023-06-12 17:44:42 +0100

    feat: Use default media path if none provided

--- backend/src/main/kotlin/com/omricat/sleepystories/server/Main.kt
+++ backend/src/main/kotlin/com/omricat/sleepystories/server/Main.kt
@@ -11,16 +11,16 @@ public fun main(vararg args: String) {
 
 private const val SLEEPY_STORIES_MEDIA_PATH = "SLEEPY_STORIES_MEDIA_PATH"
 
+private const val DEFAULT_MEDIA_PATH = "/media"
+
 @SpringBootApplication
 public class SleepyStoriesServer {
 
     @get:Bean
     public val mediaFileService: MediaFileService by lazy {
-        val basePath = System.getenv()[SLEEPY_STORIES_MEDIA_PATH]?.let { Path.of(it) }
-        if (basePath != null) {
-            MediaFileService.instance(basePath)
-        } else {
-            MediaFileService.NullMediaFileService
-        }
+        val basePath: Path =
+            System.getenv()[SLEEPY_STORIES_MEDIA_PATH]?.let { Path.of(it) }
+                ?: Path.of(DEFAULT_MEDIA_PATH)
+        MediaFileService.instance(basePath)
     }
 }
